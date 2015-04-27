2d13d986eab32f7669a34d856f7a5e29be150ddb
Timo Koepke <timo.koepke@googlemail.com>
2015-04-27 21:58:52 +0200

    Add main class

---
+++ src/main/java/eu/lindenbaum/kata/fringe/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package eu.lindenbaum.kata.fringe;
+
+/**
+ * @author timo
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Fringe");
+
+  }
+
+}
