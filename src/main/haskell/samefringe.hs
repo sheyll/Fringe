bdfa0c3f8bb25cd4b5c9028c71489ab9822517b7
Timo Koepke <timo.koepke@googlemail.com>
2015-05-04 23:34:53 +0200

    Add generators and tests

--- src/main/haskell/samefringe.hs
+++ src/main/haskell/samefringe.hs
@@ -1,4 +1,5 @@
 data Tree a = Leaf a | Node (Tree a) (Tree a)
+            deriving Show
 
 leaves :: Tree a -> [a]
 leaves (Node left right) = leaves left ++ leaves right
@@ -6,3 +7,31 @@ leaves (Leaf a) = [a]
 
 sameFringe :: (Eq a) => Tree a -> Tree a -> Bool
 sameFringe a b = leaves a == leaves b
+
+generateRightishTree :: Int -> Tree Int
+generateRightishTree size =
+  generate 0
+  where generate current =
+          if current < size
+          then
+            Node (Leaf current) (generate (current + 1))
+          else
+            Leaf size
+
+generateLeftishTree :: Int -> Tree Int
+generateLeftishTree size =
+  if size > 0
+  then
+    Node (generateLeftishTree (size - 1)) (Leaf size)
+  else
+    Leaf 0
+
+size :: Int
+size = 10000
+
+main :: IO()
+main = do
+  putStrLn (show (sameFringe (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0)))))
+  putStrLn (show (sameFringe (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0)))))
+  putStrLn (show (sameFringe (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0)))))
+  putStrLn (show (sameFringe (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0)))))
