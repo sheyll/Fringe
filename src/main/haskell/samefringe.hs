9fe2c447c49970326c5e5a0ddb6faca8c370fd7c
Timo Koepke <LTI2000@users.noreply.github.com>
2015-05-06 23:54:47 +0200

    Update samefringe.hs

--- src/main/haskell/samefringe.hs
+++ src/main/haskell/samefringe.hs
@@ -2,8 +2,8 @@ data Tree a = Leaf a | Node (Tree a) (Tree a)
             deriving Show
 
 leaves :: Tree a -> [a]
+leaves (Leaf a)          = [a]
 leaves (Node left right) = leaves left ++ leaves right
-leaves (Leaf a) = [a]
 
 sameFringe :: (Eq a) => Tree a -> Tree a -> Bool
 sameFringe a b = leaves a == leaves b
