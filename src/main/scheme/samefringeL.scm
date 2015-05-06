1b9d37121823817537483055b4fb21f0f78f1ad6
timo <timo.koepke@lindenbaum.eu>
2015-05-06 21:09:09 +0200

    Fiddle with samefringeL.scm

--- src/main/scheme/samefringeL.scm
+++ src/main/scheme/samefringeL.scm
@@ -8,32 +8,9 @@
 
 ;; continuations
 
-(define *depth* 0)
-(define *max-depth* 0)
-(define *ticks* 0)
-(define *answer* '())
-
-(define (build-cont f)
-  (set! *depth* (+ *depth* 1))
-  (set! *max-depth* (max *depth* *max-depth*))
-  f)
-
-(define (bounce thunk)
-  (if (procedure? thunk)
-      (bounce (thunk))
-      thunk))
-
-(define (apply-cont k val)
-  (lambda ()
-    (set! *depth* (- *depth* 1))
-    (set! *ticks* (+ *ticks* 1))
-    (k val)))
-
 ;; lists
 
 (define pair*? pair?)
-
-(define null*? null?)
 
 (define cons* cons)
 
@@ -62,54 +39,45 @@
 (define node->right cdr)
 
 ; concat*
-; CPS style
-(define (concat*K l1 l2 k)
-  (if (pair*? (l1))
-      (concat*K (lambda () (cdr* (l1))) l2 (build-cont (lambda (l3)
-                                                         (apply-cont k (lambda () (cons* (lambda () (car* (l1))) l3))))))
-      (apply-cont k l2)))
+; direct style
+(define (concat* l1 l2)
+  (let ((l1* (l1)))
+    (display "l1* ")(display l1*)(newline)
+    (if (null? l1*)
+        l2
+        (lambda () (cons* (car* l1*) (concat* (cdr* l1*) l2))))))
 
 ; leaves
-; CPS style
-(define (leavesK x k)
-  (cond ((leaf? x)
-         (apply-cont k (cons* (lambda () (leaf->label x)) (lambda () '()))))
-        ((node? x)
-         (leavesK (node->left x) (build-cont (lambda (l)
-                                               (leavesK (node->right x) (build-cont (lambda (r)
-                                                                                      (concat*K (lambda () l) (lambda () r) k))))))))))
+; direct style
+(define (leaves x)
+  (let ((x* (x)))
+    (display "x* ")(display x*)(newline)
+    (cond ((leaf? x*)
+           (lambda () (cons* (lambda () (leaf->label x*)) (lambda ()'()))))
+          ((node? x*)
+           (concat* (leaves (lambda () (node->left x*)))
+                    (leaves (lambda () (node->right x*))))))))
 
 ; same?
-; CPS style
-(define (same?K a b k)
-  (if (and (pair*? a) (pair*? b))
-      (same?K (car* a) (car* b) (build-cont (lambda (e?)
-                                              (if e?
-                                                  (same?K (cdr* a) (cdr* b) k)
-                                                  (apply-cont k #f)))))
-      (apply-cont k (equal? a b))))
+; direct style
+(define (same? a b)
+  (let ((a* (a))
+        (b* (b)))
+    (display "a* ")(display a*)(newline)
+    (display "b* ")(display b*)(newline)
+    (if (and (pair*? a*) (pair*? b*))
+        (and (same? (car* a*) (car* b*))
+             (same? (cdr* a*) (cdr* b*)))
+        (equal? a* b*))))
 
 ; samefringe
 ; CPS style
-(define (sameFringeK a b k)
-  (leavesK a (build-cont (lambda (l1)
-                           (leavesK b (build-cont (lambda (l2)
-                                                    (same?K l1 l2 k))))))))
 
 ; samefringe
 ; direct style
 (define (sameFringe a b)
-  (set! *depth* 0)
-  (set! *max-depth* 0)
-  (set! *ticks* 0)
-  (bounce
-   (sameFringeK a b (build-cont (lambda (answer)
-                                  (set! *answer* answer)
-                                  (display " depth:     ") (display *depth*) (newline)
-                                  (display " max-depth: ") (display *max-depth*) (newline)
-                                  (display " ticks:     ") (display *ticks*) (newline)
-                                  '()))))
-  *answer*)
+  (same? (leaves (lambda () a))
+         (leaves (lambda () b))))
 
 ; generator
 (define (generateRightishTree size)
@@ -126,7 +94,7 @@
 
 ;; tests
 
-(define size 2500)
+(define size 10)
 
 (define (time f)
   (let* ((start-time (current-milliseconds))
@@ -145,92 +113,12 @@
                 (begin (display "*FAILURE*") (newline))))))
   (newline))
 
-(define (show x)
-  (if (pair? (x))
-      (begin
-        (display "(")
-        (show (car (x)))
-        (display " . ")
-        (show (cdr (x)))
-        (display ")"))
-      (display (x))))
 
+(test "same leaves"       #t   (Leaf 1)                                                      (Leaf 1))
+(test "different leaves"  #f   (Leaf 1)                                                      (Leaf 2))
+(test "same trees"        #t   (Node (Leaf 1) (Node (Leaf 2) (Leaf 3)))                      (Node (Node (Leaf 1) (Leaf 2)) (Leaf 3)))
 
-
-;(show (lambda () '()))
-;(newline)
-;
-;(show (lambda () (cons (lambda () 'a) (lambda () '()))))
-;(newline)
-;
-;(show (lambda () (cons (lambda () '()) (lambda () 'b))))
-;(newline)
-;
-;(show (lambda () (cons (lambda () 'a) (lambda () 'b))))
-;(newline)
-
-(show
- (bounce
-  (concat*K (lambda ()
-              '())
-            (lambda () 
-              '())
-            (build-cont (lambda (answer)
-                          answer)))))
-
-(show 
- (bounce
-  (concat*K (lambda ()
-              '())
-            (lambda () 
-              (cons*
-               (lambda ()
-                 'b)
-               (lambda ()
-                 '())))
-            (build-cont (lambda (answer)
-                          answer)))))
-
-(show 
- (bounce
-  (concat*K (lambda ()
-              (cons*
-               (lambda ()
-                 'a) 
-               (lambda ()
-                 '())))
-            (lambda () 
-              '())
-            (build-cont (lambda (answer)
-                          answer)))))
-
-(show 
- (bounce
-  (concat*K (lambda ()
-              (cons*
-               (lambda ()
-                 'a) 
-               (lambda ()
-                 '())))
-            (lambda () 
-              (cons*
-               (lambda ()
-                 'b)
-               (lambda ()
-                 '())))
-            (build-cont (lambda (answer)
-                          answer)))))
-
-;(test "same leaves"       #t   (Leaf 1)                                           (Leaf 1))
-;(test "different leaves"  #f   (Leaf 1)                                           (Leaf 2))
-;(test "same trees"        #t   (Node (Leaf 1) (Node (Leaf 2) (Leaf 3)))           (Node (Node (Leaf 1) (Leaf 2)) (Leaf 3)))
-;
-;(test "rightish/rightish" #t   (Node (Leaf 0) (Node (generateRightishTree size)   (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
-;(test "leftish/leftish"   #t   (Node (Leaf 0) (Node (generateLeftishTree size)    (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
-;(test "rightish/leftish"  #t   (Node (Leaf 0) (Node (generateRightishTree size)   (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
-;(test "leftish/rightish"  #t   (Node (Leaf 0) (Node (generateLeftishTree size)    (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
-;
-;(test "rightish/rightish different first" #t   (Node (Leaf 1) (Node (generateRightishTree size)   (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
-;(test "leftish/leftish different first"   #t   (Node (Leaf 2) (Node (generateLeftishTree size)    (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
-;(test "rightish/leftish different first"  #t   (Node (Leaf 3) (Node (generateRightishTree size)   (Leaf 0))) (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
-;(test "leftish/rightish different first"  #t   (Node (Leaf 4) (Node (generateLeftishTree size)    (Leaf 0))) (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
+(test "rightish/rightish" #f   (Node (Leaf 1) (Node (generateRightishTree size) (Leaf 0)))   (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
+(test "leftish/leftish"   #f   (Node (Leaf 2) (Node (generateLeftishTree size)  (Leaf 0)))   (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
+(test "rightish/leftish"  #f   (Node (Leaf 3) (Node (generateRightishTree size) (Leaf 0)))   (Node (Leaf 0) (Node (generateLeftishTree size)  (Leaf 0))))
+(test "leftish/rightish"  #f   (Node (Leaf 4) (Node (generateLeftishTree size)  (Leaf 0)))   (Node (Leaf 0) (Node (generateRightishTree size) (Leaf 0))))
