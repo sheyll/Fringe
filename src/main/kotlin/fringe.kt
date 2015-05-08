ac8962196f6ab07d142be46bfe0e73e4824b7b2e
Rusi Filipov <rusi.filipov@lindenbaum.eu>
2015-05-08 18:02:14 +0200

    Convert base Tree from abstract class to a trait

--- src/main/kotlin/fringe.kt
+++ src/main/kotlin/fringe.kt
@@ -1,16 +1,16 @@
 package com.github.rusio
 
-abstract class Tree {
-  abstract fun fringe(): List<Tree>
+trait Tree {
+  fun fringe(): List<Tree>
 }
 
-data class Leaf(val value: Int) : Tree() {
+data class Leaf(val value: Int) : Tree {
   override fun fringe(): List<Tree> {
     return listOf(this)
   }
 }
 
-data class Node(val a: Tree, val b: Tree) : Tree() {
+data class Node(val a: Tree, val b: Tree) : Tree {
   override fun fringe(): List<Tree> {
     return a.fringe() + b.fringe()
   }
